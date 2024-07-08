0e10458043fa9dbafb19f704fdf6f2e85edc9471
Joseph Smith <joseph.lochlann@gmail.com>
2024-07-08 11:57:29 +0100

    LinkedQueue

--- 05-stacks-and-queues.cpp
+++ 05-stacks-and-queues.cpp
@@ -160,6 +160,9 @@ class LinkedStack {
   }
 
   int pop() {
+    if (!this->top) {
+      return -1;
+    }
     // The pop function should remove data from the top of the stack
     Link *oldTop = this->top;
     this->top = this->top->getNext();
@@ -170,6 +173,9 @@ class LinkedStack {
 
   int peek() {
     // The peek function should return the top of the stack (but not delete)
+    if (!this->top) {
+      return -1;
+    }
     return this->top->getData();
   }
 };
@@ -206,29 +212,56 @@ class LinkedStack {
 class Queue {
  private:
   // declare a constant for the MAX_SIZE of the queue
+  static const size_t MAX_SIZE = 10;
   // define an integer array of MAX_SIZE to store elements
+  int elements[MAX_SIZE];
   // define an variable that holds the index of the 'front' element
+  int front = 0;
   // define an variable that holds the index of the 'rear' element
+  int rear = 0;
 
  public:
   Queue() {}
 
-  bool isEmpty() { return 0; }
+  bool isEmpty() { return front == rear; }
 
-  bool isFull() { return 0; }
+  bool isFull() { return front == (rear + 1) % MAX_SIZE; }
 
   void enqueue(int value) {
     // The enqueue function should add data to the 'rear' of the queue.
+
+    // Example:
+    // max_size 1, r 0, f 0
+    // []
+    // not full => set el[0], r becomes 1
+    // [1]
+    // next enqueue: max_size 1 = r 1, array is full (no-op)
+    // cout << "enqueue: " << value << endl;
+    // cout << "isFull: " << isFull() << endl;
+    if (!isFull()) {
+      // cout << "adding at rear: " << rear << endl;
+      elements[rear] = value;
+      rear = (rear + 1) % MAX_SIZE;
+      // cout << "new rear: " << rear << endl;
+    }
   }
 
   int dequeue() {
     // The dequeue function should remove data to the 'front' of the queue.
-    return 0;
+    // [1, 2]
+    if (!isEmpty()) {
+      // cout << "removing at front: " << front << endl;
+      int ret = elements[front];
+      front = (front + 1) % MAX_SIZE;
+      // cout << "new front: " << front << endl;
+      return ret;
+    }
+    return -1;
   }
 
   int peek() {
     // The peek function should return the front of the queue (but not delete)
-    return 0;
+    return elements[front];
   }
 };
 
@@ -253,29 +286,61 @@ class Queue {
 class LinkedQueue {
  private:
   // define a pointer to the front of the queue
+  Link *front;
   // define a pointer to the rear of the queue
+  Link *rear;
 
  public:
   LinkedQueue() {}
 
   ~LinkedQueue() {  // Destructor!
-                    // Delete all nodes to avoid memory leaks
+    while (!isEmpty()) {
+      dequeue();
+    }
   }
 
-  bool isEmpty() { return 0; }
+  bool isEmpty() {
+    // check rear too?
+    if (!front) {
+      return true;
+    }
+  }
 
-  void enqueue(int value) {  // feel free to amend this signature
-    // The enqueue function should add data to the 'rear' of the queue.
+  void enqueue(int value) {
+    // Create new link with value
+    Link *newLink = new Link(value);
+    // Empty is a special case:
+    // - set front and rear to new link
+    // Otherwise:
+    // - new link is new rear
+    // - old rear next => new link
+    if (isEmpty()) {
+      front = rear = newLink;
+    } else {
+      rear->setNext(newLink);
+      rear = newLink;
+    }
   }
 
   int dequeue() {  // feel free to amend the return type
     // The dequeue function should remove data to the 'front' of the queue.
-    return 0;
+    if (isEmpty()) {
+      return -1;
+    }
+    Link *oldFront = front;
+    int ret = oldFront->getData();
+    Link *newFront = oldFront->getNext();
+    front = newFront;
+    delete oldFront;
+    return ret;
   }
 
   int peek() {  // feel free to amend the return type
     // The peek function should return the front of the queue (but not delete)
-    return 0;
+    if (isEmpty()) {
+      return -1;
+    }
+    return front->getData();
   }
 };
 
@@ -303,23 +368,23 @@ int main() {
   cout << "Exercise 1: Stack using an Array / Vector" << endl;
   // instantiate the Stack class and push/pop values here.
 
-  // ArrayStack *arrayStack = new ArrayStack();
-  // cout << "peeked " << arrayStack->peek().value_or(-999) << endl;
-  // arrayStack->push(1);
-  // arrayStack->push(2);
-  // arrayStack->push(3);
-  // cout << "peeked " << arrayStack->peek().value_or(-999) << endl;
-  // cout << "popped " << arrayStack->pop().value_or(-999) << endl;
-  // cout << "popped " << arrayStack->pop().value_or(-999) << endl;
-  // cout << "popped " << arrayStack->pop().value_or(-999) << endl;
-  // cout << "popped " << arrayStack->pop().value_or(-999) << endl;
-  // cout << "popped " << arrayStack->pop().value_or(-999) << endl;
+  ArrayStack *arrayStack = new ArrayStack();
+  cout << "peeked " << arrayStack->peek().value_or(-999) << endl;
+  arrayStack->push(1);
+  arrayStack->push(2);
+  arrayStack->push(3);
+  cout << "peeked " << arrayStack->peek().value_or(-999) << endl;
+  cout << "popped " << arrayStack->pop().value_or(-999) << endl;
+  cout << "popped " << arrayStack->pop().value_or(-999) << endl;
+  cout << "popped " << arrayStack->pop().value_or(-999) << endl;
+  cout << "popped " << arrayStack->pop().value_or(-999) << endl;
+  cout << "popped " << arrayStack->pop().value_or(-999) << endl;
 
   // Ex 2 - Stack with a Linked List
   cout << "\nExercise 2: Stack using a Linked List " << endl;
 
   LinkedStack *linkedStack = new LinkedStack();
-  // cout << "peeked " << linkedStack->peek() << endl;
+  cout << "peeked " << linkedStack->peek() << endl;
   linkedStack->push(1);
   linkedStack->push(2);
   linkedStack->push(3);
@@ -328,20 +393,88 @@ int main() {
   cout << "popped " << linkedStack->pop() << endl;
   cout << "popped " << linkedStack->pop() << endl;
   cout << "peeked " << linkedStack->peek() << endl;
-  // cout << "popped " << linkedStack->pop() << endl;
-  // cout << "popped " << linkedStack->pop() << endl;
-  // cout << "popped " << linkedStack->pop() << endl;
+  cout << "popped " << linkedStack->pop() << endl;
+  cout << "popped " << linkedStack->pop() << endl;
+  cout << "popped " << linkedStack->pop() << endl;
 
   // Ex 3 - Queue using an Array
 
   cout << "\nExercise 3: Queue using an Array " << endl;
+  Queue *queue = new Queue();
+  // Seems like we have no way of distinguishing between 0 and uninitialized
+  cout << "peeked (empty)" << queue->peek() << endl;
+  queue->enqueue(1);
+  queue->enqueue(2);
+  queue->enqueue(3);
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  queue->enqueue(1);
+  queue->enqueue(2);
+  queue->enqueue(3);
+  queue->enqueue(4);
+  queue->enqueue(5);
+  queue->enqueue(6);
+  queue->enqueue(7);
+  queue->enqueue(8);
+  queue->enqueue(9);
+  queue->enqueue(10);
+  queue->enqueue(11);
+  queue->enqueue(12);
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+  cout << "dequeue " << queue->dequeue() << endl;
+
 
   // instantiate the Queue class and enqueue/dequeue values here.
 
   // Ex 4 - Queue using a Linked List
   cout << "\nExercise 4: Queue using a Linked List " << endl;
-
-  // instantiate the LinkedQueue class and enqueue/dequeue values here.
+  LinkedQueue *linkedQueue = new LinkedQueue();
+  // Seems like we have no way of distinguishing between 0 and uninitialized
+  cout << "peeked (empty)" << linkedQueue->peek() << endl;
+  cout << "dequeue (empty)" << linkedQueue->dequeue() << endl;
+  linkedQueue->enqueue(1);
+  linkedQueue->enqueue(2);
+  linkedQueue->enqueue(3);
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  linkedQueue->enqueue(1);
+  linkedQueue->enqueue(2);
+  linkedQueue->enqueue(3);
+  linkedQueue->enqueue(4);
+  linkedQueue->enqueue(5);
+  linkedQueue->enqueue(6);
+  linkedQueue->enqueue(7);
+  linkedQueue->enqueue(8);
+  linkedQueue->enqueue(9);
+  linkedQueue->enqueue(10);
+  linkedQueue->enqueue(11);
+  linkedQueue->enqueue(12);
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
+  cout << "dequeue " << linkedQueue->dequeue() << endl;
 
   // Ex 5 - STL Stack and Queue
 
